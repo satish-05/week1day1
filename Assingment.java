08fb3a4974a3bc98448cfced2a7b69dfb422693e
satish-05 <90094992+satish-05@users.noreply.github.com>
2021-09-04 19:09:42 +0530

    Add files via upload

---
+++ Assingment.java
@@ -0,0 +1,24 @@
+package week1.day1;
+
+public class Assingment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   
+		int num = 35;
+		
+		if (num>0) {
+			System.out.println("The Number Is Positive");
+		}
+			
+			else if (num<0) {
+				System.out.println("The Number Is Negative");
+			}
+	     else {
+				System.out.println("The Number Is Neither Positive nor Negative");
+			}
+		}
+   
+	}
+
+
---
+++ Assingment2.java
@@ -0,0 +1,13 @@
+package week1.day1;
+
+public class Assingment2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int num = -40;
+        if(num<0)
+        	System.out.println(-num);
+        
+	}
+
+}
